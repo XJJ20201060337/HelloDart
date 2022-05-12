952ef66b59ae62d598ab2ee21da56435dd523742
XJJ20201060337 <xjj18107971497@163.com>
2022-05-12 09:34:52 +0000

    Dart-变量

---
+++ test2.2/02_variable.dart
@@ -0,0 +1,11 @@
+ void main(List<String> args) {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter','Saturn','Uranus','Neptune'];
+  var image = {
+    'tags':['saturn'],
+    'url':'//path/to/saturn.jpg'
+  };
+  print('The name is $name,the year is $year,the antennaDiameter is $antennaDiameter,the flyObjects is $flybyObjects,the image is $image');
+}
