65d270cccb309551e224df3b65f22cb7cffd005a
XJJ20201060337 <xjj18107971497@163.com>
2022-05-26 08:27:05 +0000

    新增了Mixins的使用

--- test2.2/05_Class.dart
+++ test2.2/05_Class.dart
@@ -1,30 +1,47 @@
-class Spacecraft{
+class Spacecraft {
   String name;
-  DateTime?launchDate;
-  int? get launchYear =>launchDate?.year;
-  Spacecraft(this.name,this.launchDate){}
-  Spacecraft.unlaunched(String name):this(name,null);
-  void describe(){
+  DateTime? launchDate;
+  int? get launchYear => launchDate?.year;
+  Spacecraft(this.name, this.launchDate) {}
+  Spacecraft.unlaunched(String name) : this(name, null);
+  void describe() {
     print('Spacecraft:$name');
-    var launchDate=this.launchDate;
-    if(launchDate!=null){
-      int years = DateTime.now().difference(launchDate).inDays~/365;
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
       print('Launched:$launchYear($years years ago)');
-    }else{
+    } else {
       print('Unlaunched');
     }
   }
 }
 
-class Orbiter extends Spacecraft{
+class Orbiter extends Spacecraft {
   double altitude;
-  Orbiter(String name,DateTime launchDate,this.altitude):super(name,launchDate);
+  Orbiter(String name, DateTime launchDate, this.altitude)
+      : super(name, launchDate);
 }
 
-void main(){
-  var voyager = Spacecraft('Voyager I',DateTime(1977,9,5));
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts:$astronauts');
+  }
+}
+
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(String name, DateTime launchDate) : super(name, launchDate);
+}
+
+void main() {
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
   voyager.describe();
 
   var voyager3 = Spacecraft.unlaunched('Voyager III');
   voyager3.describe();
+
+  var ptl = PilotedCraft('Voyager II', DateTime(1977, 8, 20));
+  ptl.describe();
+  ptl.describeCrew();
 }
