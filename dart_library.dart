42fa120152f14d48a527a39ac037f397985cc03c
XJJ20201060337 <xjj18107971497@163.com>
2022-06-29 09:56:58 +0000

    新增了异常

--- dart_library.dart
+++ dart_library.dart
@@ -1,4 +1,40 @@
 import 'dart:core';
+import 'dart:async';
+class Line implements Comparable<Line> {
+  final int length;
+  const Line(this.length);
+
+  @override
+  int compareTo(Line other) => length - other.length;
+}
+
+class Person {
+  final String firstName, lastName;
+
+  Person(this.firstName, this.lastName);
+
+  // 使用由"对象类"提供的静态哈希方法覆盖哈希代码
+  @override
+  int get hashCode => Object.hash(firstName, lastName);
+
+  // You should generally implement operator `==` if you
+  // override `hashCode`.
+  @override
+  bool operator ==(dynamic other) {
+    return other is Person &&
+        other.firstName == firstName &&
+        other.lastName == lastName;
+  }
+}
+
+class FooException implements Exception {
+  final String? msg;
+
+  const FooException([this.msg]);
+
+  @override
+  String toString() => msg ?? 'FooException';
+}
 
 void dartcore() {
   print('\n');
@@ -274,47 +310,123 @@ void dartcore() {
   //声明一个Map类型
   //Map通常使用字符串作为键值
   var hawaiianBeaches = {
-    'Oahu':['Waikiki','Kailua','Waimanalo'],
-    'Big Island':['Wailea Bay','Pololu Beach'],
-    'Kauai':['Hanalei','Poipu']
+    'Oahu': ['Waikiki', 'Kailua', 'Waimanalo'],
+    'Big Island': ['Wailea Bay', 'Pololu Beach'],
+    'Kauai': ['Hanalei', 'Poipu']
   };
   //Maps能从构造函数生成映射
   var seacherTerms = Map();
   //Maps是一种参数化类型，你可以任意指定键值的类型
-  var nobleGases = Map<int,String>();
   //通过大括号语法可以为map添加、获取、设置元素
+  var nobleGases_Map = Map<int, String>();
   //使用remove()方法可以从map中移除键值对
-  var nobleGases = {54:'xemon'};
-  print(nobleGases[54]);
-  print(nobleGases.containsKey(54));
+  nobleGases_Map = {54: 'xemon'};
+  print(nobleGases_Map[54]);
+  print(nobleGases_Map.containsKey(54));
   nobleGases.remove(54);
-  if(!nobleGases.containsKey(54)){
+  if (!nobleGases_Map.containsKey(54)) {
     print('nobleGases已被移除');
   }
   //检索map所有的key或者value
   var keys = hawaiianBeaches.keys;
   print(keys.length);
-  if(Set.from(keys).contains('Oahu')){
+  if (Set.from(keys).contains('Oahu')) {
     print('键集合keys包含元素“Oahu”');
-  }
-  else{
+  } else {
     print('键集合keys不包含元素“Oahu”');
   }
   var values = hawaiianBeaches.values;
   print(values.length);
-  //使用 containsKey() 方法检查一个 map 中是否包含某个key 
-  if(hawaiianBeaches.containsKey('Oahu')){
+  //使用 containsKey() 方法检查一个 map 中是否包含某个key
+  if (hawaiianBeaches.containsKey('Oahu')) {
     print('键集合keys包含元素“Oahu”');
-  }else{
+  } else {
     print('键集合keys不包含元素“Oahu”');
   }
-  if(!hawaiianBeaches.containsKey('Florida')){
+  if (!hawaiianBeaches.containsKey('Florida')) {
     print('键集合keys不包含元素“Florida”');
-  }else{
-    print('键集合keys包含元素“Florida”')
+  } else {
+    print('键集合keys包含元素“Florida”');
   }
-  var teamAssignments = <String,String>{};
+  var teamAssignments = <String, String>{};
   teamAssignments.putIfAbsent('Catcher', () => pickToughestKid());
+  print('*' * 40);
+
+  print('\n');
+  print('*' * 40);
+  var uri = 'https://example.org/api?foo=some message';
+  var encoded = Uri.encodeFull(uri);
+  print(encoded);
+  var decoded = Uri.decodeFull(encoded);
+  var encoded1 = Uri.encodeComponent(uri);
+  print(encoded1);
+  var decoded1 = Uri.decodeComponent(encoded1);
+  if (uri == decoded1) {
+    print('uri与decoded1相同');
+  }
+  var uri1 = Uri.parse('https://example.org:8080/foo/bar#frag');
+  print('uri1=$uri1');
+  print('uri1.scheme=');
+  print(uri1.scheme);
+  print('uri1.host=');
+  print(uri1.host);
+  print('uri1.path=');
+  print(uri1.path);
+  print('uri1.fragment=');
+  print(uri1.fragment);
+  print('uri1.origin=');
+  print(uri1.origin);
+  var uri2 = Uri(
+    scheme: 'https',
+    host: 'example.org',
+    path: '/foo/bar',
+    fragment: 'frag',
+  );
+  print(uri2.toString());
+  print('*' * 40);
+
+  print('\n');
+  print('*' * 40);
+  print('日期和时间');
+  var now = DateTime.now();
+  var y2k = DateTime(2000);
+  var unixEpoch = DateTime.utc(1970);
+  y2k = DateTime(2000, 1, 2);
+  y2k = DateTime.utc(2000);
+  y2k = DateTime.fromMillisecondsSinceEpoch(946684800000, isUtc: true);
+  y2k = DateTime.parse('2000-01-01T00:00:00Z');
+
+  print(y2k.millisecondsSinceEpoch);
+  print(unixEpoch.millisecondsSinceEpoch);
+  var y2001 = y2k.add(const Duration(days: 366));
+  print(y2001.year);
+  var december2000 = y2001.subtract(const Duration(days: 30));
+  print(december2000.year);
+  print(december2000.month);
+  var duration = y2001.difference(y2k);
+  print(duration.inDays);
+  print('*' * 40);
+
+  print('\n');
+  print('*' * 40);
+  print('工具类');
+  var short = const Line(1);
+  var long = const Line(100);
+  print(short.compareTo(long));
+  var p1 = Person('Bob', 'Smith');
+  var p2 = Person('Bob', 'Smith');
+  var p3 = 'not a person';
+  if (p1.hashCode == p2.hashCode) {
+    print('p1的哈希代码与p2的哈希代码相同');
+  }
+  if (p1 == p2) print('p1与p2相同');
+  if (p1 != p3) print('p1与p3不相同');
+  print('*' * 40);
+
+  print('\n');
+  print('*' * 40);
+  print('异常');
+
   print('*' * 40);
 }
 
